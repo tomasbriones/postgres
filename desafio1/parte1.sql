8f5903c43e3c741d5d23fb38a29595ca4c68b96d
Tomas Briones <tomasbriones@gmail.com>
2021-03-31 20:18:38 -0300

    new column title and update

--- desafio1/parte1.sql
+++ desafio1/parte1.sql
@@ -14,3 +14,11 @@ INSERT INTO posts(nombre_usuario,fecha_creacion,contenido,descripcion) VALUES('P
 -- 1 | Pamela         | 2021-03-31     | mi foto de onstigem   | aqui agregue mi foto
 -- 2 | Pamela         | 2021-03-31     | mi perfil de onstigem | aqui agregue mi perfil
 -- 3 | Carlos         | 2021-03-31     | mi foto de fecibuk    | aqui agregue mi foto
+
+------Agregar Columna nueva------
+ALTER TABLE posts ADD titulo VARCHAR(30);
+
+------Agregar titulos a filas------
+UPDATE posts SET titulo = 'pamela post1' WHERE id = 1;
+UPDATE posts SET titulo = 'pamela post2' WHERE id = 2;
+UPDATE posts SET titulo = 'carlos post1' WHERE id = 3;
