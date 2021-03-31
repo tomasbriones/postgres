c4a8ec228efa0c0358da32f4b68003e1a07cda9a
Tomas Briones <tomasbriones@gmail.com>
2021-03-31 20:39:32 -0300

    2 new posts

--- desafio1/parte1.sql
+++ desafio1/parte1.sql
@@ -16,9 +16,26 @@ INSERT INTO posts(nombre_usuario,fecha_creacion,contenido,descripcion) VALUES('P
 -- 3 | Carlos         | 2021-03-31     | mi foto de fecibuk    | aqui agregue mi foto
 
 ------Agregar Columna nueva------
-ALTER TABLE posts ADD titulo VARCHAR(30);
+ALTER TABLE posts ADD COLUMN titulo VARCHAR(30);
 
 ------Agregar titulos a filas------
 UPDATE posts SET titulo = 'pamela post1' WHERE id = 1;
 UPDATE posts SET titulo = 'pamela post2' WHERE id = 2;
 UPDATE posts SET titulo = 'carlos post1' WHERE id = 3;
+
+ --id | nombre_usuario | fecha_creacion |       contenido       |      descripcion       |    titulo    
+------+----------------+----------------+-----------------------+------------------------+--------------
+  --1 | Pamela         | 2021-03-31     | mi foto de onstigem   | aqui agregue mi foto   | pamela post1
+  --2 | Pamela         | 2021-03-31     | mi perfil de onstigem | aqui agregue mi perfil | pamela post2
+  --3 | Carlos         | 2021-03-31     | mi foto de fecibuk    | aqui agregue mi foto   | carlos post1
+
+------Insertar 2 posts------
+INSERT INTO posts(nombre_usuario,fecha_creacion,contenido,descripcion,titulo) VALUES('Pedro','2021-04-01','mi foto de onstigem','aqui agregue mi foto','post pedro1'),('Pedro','2021-04-01','mi perfil de onstigem','aqui agregue mi perfil','post pedro2');
+
+ --id | nombre_usuario | fecha_creacion |       contenido       |      descripcion       |    titulo    
+------+----------------+----------------+-----------------------+------------------------+--------------
+  --1 | Pamela         | 2021-03-31     | mi foto de onstigem   | aqui agregue mi foto   | pamela post1
+  --2 | Pamela         | 2021-03-31     | mi perfil de onstigem | aqui agregue mi perfil | pamela post2
+  --3 | Carlos         | 2021-03-31     | mi foto de fecibuk    | aqui agregue mi foto   | carlos post1
+  --4 | Pedro          | 2021-04-01     | mi foto de onstigem   | aqui agregue mi foto   | post pedro1
+  --5 | Pedro          | 2021-04-01     | mi perfil de onstigem | aqui agregue mi perfil | post pedro2
