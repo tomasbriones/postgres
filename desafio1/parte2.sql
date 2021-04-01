410c00e1e6ca546f4db97559c9886ffd786ab1b0
Tomas Briones <tomasbriones@gmail.com>
2021-03-31 21:48:57 -0300

    insert pamela and carlos values

--- desafio1/parte2.sql
+++ desafio1/parte2.sql
@@ -7,3 +7,15 @@ ALTER TABLE comentarios ADD FOREIGN KEY (id) REFERENCES posts (id);
 
  --id | fecha | hora | contenido 
 ------+-------+------+-----------
+
+------Insertar comentarios a pamela y carlos------
+INSERT INTO comentarios (id,fecha,hora,contenido) VALUES ('1','2021-03-30','12:01:00','Aqui escrio algo'),('1','2021-03-30','12:01:00','Aqui escrio algo'),('6','2021-03-30','19:01:00','Aqui escrio algo'),('6','2021-03-30','16:01:00','Aqui escrio algo'),('6','2021-05-30','16:01:00','Aqui escrio algo'),('6','2021-04-30','16:01:00','Aqui escrio algo');
+
+ --id |   fecha    |   hora   |    contenido     
+------+------------+----------+------------------
+  --1 | 2021-03-30 | 12:01:00 | Aqui escrio algo
+  --1 | 2021-03-30 | 12:01:00 | Aqui escrio algo
+  --6 | 2021-03-30 | 19:01:00 | Aqui escrio algo
+  --6 | 2021-03-30 | 16:01:00 | Aqui escrio algo
+  --6 | 2021-05-30 | 16:01:00 | Aqui escrio algo
+  --6 | 2021-04-30 | 16:01:00 | Aqui escrio algo
