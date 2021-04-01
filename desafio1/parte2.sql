0f84313683e0a3997ba7bdb1935f357f32215649
Tomas Briones <tomasbriones@gmail.com>
2021-03-31 21:55:57 -0300

    add margarita post

--- desafio1/parte2.sql
+++ desafio1/parte2.sql
@@ -19,3 +19,15 @@ INSERT INTO comentarios (id,fecha,hora,contenido) VALUES ('1','2021-03-30','12:0
   --6 | 2021-03-30 | 16:01:00 | Aqui escrio algo
   --6 | 2021-05-30 | 16:01:00 | Aqui escrio algo
   --6 | 2021-04-30 | 16:01:00 | Aqui escrio algo
+
+------Crear nuevo post para margarita------
+INSERT INTO posts(nombre_usuario,fecha_creacion,contenido,descripcion,titulo) VALUES('Margarita','2021-04-05','mi foto de piterste','aqui agregue foto perfil','post margarita1');
+
+ --id | nombre_usuario | fecha_creacion |       contenido       |       descripcion        |     titulo      
+------+----------------+----------------+-----------------------+--------------------------+-----------------
+  --1 | Pamela         | 2021-03-31     | mi foto de onstigem   | aqui agregue mi foto     | pamela post1
+  --2 | Pamela         | 2021-03-31     | mi perfil de onstigem | aqui agregue mi perfil   | pamela post2
+  --4 | Pedro          | 2021-04-01     | mi foto de onstigem   | aqui agregue mi foto     | post pedro1
+  --5 | Pedro          | 2021-04-01     | mi perfil de onstigem | aqui agregue mi perfil   | post pedro2
+  --6 | Carlos         | 2021-04-01     | mi foto de onstigem   | aqui agregue mi foto     | nuevo post
+  --7 | Margarita      | 2021-04-05     | mi foto de piterste   | aqui agregue foto perfil | post margarita1
