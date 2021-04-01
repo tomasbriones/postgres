533e82288ad89b859cb1deb6123a425c97bdb48e
Tomas Briones <tomasbriones@gmail.com>
2021-03-31 21:03:13 -0300

    delete carlos post

--- desafio1/parte1.sql
+++ desafio1/parte1.sql
@@ -39,3 +39,14 @@ INSERT INTO posts(nombre_usuario,fecha_creacion,contenido,descripcion,titulo) VA
   --3 | Carlos         | 2021-03-31     | mi foto de fecibuk    | aqui agregue mi foto   | carlos post1
   --4 | Pedro          | 2021-04-01     | mi foto de onstigem   | aqui agregue mi foto   | post pedro1
   --5 | Pedro          | 2021-04-01     | mi perfil de onstigem | aqui agregue mi perfil | post pedro2
+
+------Eliminar post de carlos------
+DELETE FROM posts WHERE id = 3;
+
+ --id | nombre_usuario | fecha_creacion |       contenido       |      descripcion       |    titulo    
+------+----------------+----------------+-----------------------+------------------------+--------------
+  --1 | Pamela         | 2021-03-31     | mi foto de onstigem   | aqui agregue mi foto   | pamela post1
+  --2 | Pamela         | 2021-03-31     | mi perfil de onstigem | aqui agregue mi perfil | pamela post2
+  --4 | Pedro          | 2021-04-01     | mi foto de onstigem   | aqui agregue mi foto   | post pedro1
+  --5 | Pedro          | 2021-04-01     | mi perfil de onstigem | aqui agregue mi perfil | post pedro2
+
