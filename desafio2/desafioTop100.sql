625b2070a2265230cfebd42c39fd7709c14f4a24
Tomas Briones <tomasbriones@gmail.com>
2021-04-02 14:44:43 -0300

    adding table print

--- desafio2/desafioTop100.sql
+++ desafio2/desafioTop100.sql
@@ -17,6 +17,31 @@ CREATE TABLE repartos (id INT, actor VARCHAR(40),FOREIGN KEY (id) REFERENCES pel
 
 --listar todo el reparto de la pelicula Titanic----
 SELECT * FROM peliculas as pe JOIN repartos as re ON re.id = pe.id WHERE pe.pelicula = 'Titanic';
+-- id | pelicula | año_estreno |   director    | id |       actor
+------+----------+-------------+---------------+----+-------------------
+--  2 | Titanic  |        1997 | James Cameron |  2 | Leonardo DiCaprio
+--  2 | Titanic  |        1997 | James Cameron |  2 | Kate Winslet
+--  2 | Titanic  |        1997 | James Cameron |  2 | Billy Zane
+--  2 | Titanic  |        1997 | James Cameron |  2 | Kathy Bates
+--  2 | Titanic  |        1997 | James Cameron |  2 | Frances Fisher
+--  2 | Titanic  |        1997 | James Cameron |  2 | Bernard Hill
+--  2 | Titanic  |        1997 | James Cameron |  2 | Jonathan Hyde
+--  2 | Titanic  |        1997 | James Cameron |  2 | Danny Nucci
+--  2 | Titanic  |        1997 | James Cameron |  2 | David Warner
+--  2 | Titanic  |        1997 | James Cameron |  2 | Bill Paxton
+--  2 | Titanic  |        1997 | James Cameron |  2 | Gloria Stuart
+--  2 | Titanic  |        1997 | James Cameron |  2 | Victor Garber
+--  2 | Titanic  |        1997 | James Cameron |  2 | Suzy Amis
 
 ---listar titulos de pelicula donde actua Harrison Ford----
 SELECT pe.pelicula, re.actor FROM peliculas AS pe JOIN repartos as re ON re.id = pe.id WHERE re.actor = 'Harrison Ford' ;
+--                   pelicula                    |     actor
+-------------------------------------------------+---------------
+-- Star Wars. Episodio IV: Una nueva esperanza   | Harrison Ford
+-- Indiana Jones y la ├║ltima cruzada            | Harrison Ford
+-- En busca del arca perdida                     | Harrison Ford
+-- Star Wars. Episodio V: El imperio contraataca | Harrison Ford
+-- Star Wars. Episodio VI: El retorno del Jedi   | Harrison Ford
+-- Blade Runner                                  | Harrison Ford
+-- Apocalypse Now                                | Harrison Ford
+-- Indiana Jones y el templo maldito             | Harrison Ford
