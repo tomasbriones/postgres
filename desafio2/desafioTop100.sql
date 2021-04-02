c9b1170ce073eeef6785cf0782b5613500e33f4a
Tomas Briones <tomasbriones@gmail.com>
2021-04-02 14:27:38 -0300

    correction

--- desafio2/desafioTop100.sql
+++ desafio2/desafioTop100.sql
@@ -9,8 +9,9 @@ CREATE TABLE repartos (id INT, actor VARCHAR(40),FOREIGN KEY (id) REFERENCES pel
 
 ----cargar datos a ambas tablas desde csv-----
 
-\copy peliculas (id,pelicula,año,estreno,director) FROM 'C:\Users\giani\OneDrive\Escritorio\postgres\desafio2\ApoyoDesafio2\peliculas.csv' delimiter ',' CSV header;
+\copy peliculas (id,pelicula,año,estreno,director) FROM 'C:\Users\tomas\escritorio\postgres\desafio2\ApoyoDesafio2\peliculas.csv' delimiter ',' CSV header;
 --error con cantidad de bits en string, corregir con ALTER TABLE peliculas ALTER COLUMN pelicula TYPE VARCHAR(150);----
-\copy repartos (id,actor) FROM 'C:\Users\giani\OneDrive\Escritorio\postgres\desafio2\ApoyoDesafio2\reparto.csv' delimiter ',' CSV header;
---corregir Alex Angulo linea 575---
+\copy repartos (id,actor) FROM 
+\copy peliculas (id,pelicula,año,estreno,director) FROM 'C:\Users\tomas\escritorio\postgres\desafio2\ApoyoDesafio2\reparto.csv' delimiter ',' CSV header;
+--corregir Alex Angulo linea 575 porque no detecta UTF-8---
 
