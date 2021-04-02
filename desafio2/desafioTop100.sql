9dfa6f40a557dbdb17a84ed3d341e06637a5e174
Tomas Briones <tomasbriones@gmail.com>
2021-04-02 13:42:14 -0300

    create tables peliculas and repart

--- desafio2/desafioTop100.sql
+++ desafio2/desafioTop100.sql
@@ -1,3 +1,8 @@
 ----CREATE DATA BASE PELICULAS----
 
 CREATE DATABASE peliculas;
+
+----Crear tablas de peliculas y reparto----
+
+CREATE TABLE peliculas (id SERIAL PRIMARY KEY, pelicula VARCHAR(25), año INT,  estreno DATE, director VARCHAR(40));
+CREATE TABLE repartos (id INT, actor VARCHAR(40),FOREIGN KEY (id) REFERENCES peliculas(id));
