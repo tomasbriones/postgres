026d1cefd4fc7f69ab8b5fb92bd13f47db8395d0
Tomas Briones <tbriones@economial.cl>
2021-04-09 18:30:57 -0400

    tables structures

--- desafio3/caso1/caso1.sql
+++ desafio3/caso1/caso1.sql
@@ -4,3 +4,23 @@ CREATE DATABASE caso1;
 ---cambiamos a base de datos caso1---
 \c caso1;
 
+---creamos tabla de datos---
+CREATE TABLE department(
+    id_deparment SERIAL,
+    department_name VARCHAR(50),
+    PRIMARY KEY (id_deparment));
+
+CREATE TABLE workers(rut VARCHAR UNIQUE NOT NULL,
+    worker_name VARCHAR(50),
+    worker_address VARCHAR(100),
+    department_id INT,
+    PRIMARY KEY (rut),
+    FOREIGN KEY (department_id) REFERENCES department (id_deparment));
+
+CREATE TABLE salary(
+    salary_id SERIAL,
+    drive_file VARCHAR,
+    worker_rut VARCHAR,
+    PRIMARY KEY (salary_id),
+    FOREIGN KEY (worker_rut) REFERENCES workers(rut));
+
