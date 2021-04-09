1eb35f4581b9199d3a90e66fdcd5dc3506bcebdd
Tomas Briones <tbriones@economial.cl>
2021-04-09 18:14:04 -0400

    create database

---
+++ desafio3/caso1/caso1.sql
@@ -0,0 +1,6 @@
+---creamos la base de datos donde vamos a trabajar---
+CREATE DATABASE caso1;
+
+---cambiamos a base de datos caso1---
+\c caso1;
+
---
+++ desafio3/caso2/caso2.sql
@@ -0,0 +1,5 @@
+---creamos la base de datos donde vamos a trabajar---
+CREATE DATABASE caso2;
+
+---cambiamos a base de datos caso1---
+\c caso2;
