d88ae26fd495d041fef015842977772196451f34
Tomas Briones <tbriones@economial.cl>
2021-04-10 04:18:31 -0400

    create tables estructures

--- prueba/prueba.sql
+++ prueba/prueba.sql
@@ -5,10 +5,47 @@ CREATE DATABASE ventas;
 \c ventas;
 
 ---creamos tablas---
-CREATE TABLE cliente (
+CREATE TABLE client (
     id SERIAL,
     rut VARCHAR(20),
-    cliente_name VARCHAR(50),
-    adress VARCHAR(200)
+    client_name VARCHAR(50),
+    adress VARCHAR(200),
+    PRIMARY KEY (id)
 );
 
+CREATE TABLE invoice (
+    id SERIAL,
+    createt_at DATE,
+    client_id INT NOT NULL,
+    sub_total INT,
+    iva INT,
+    total INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
+
+CREATE TABLE category (
+    id SERIAL,
+    category_name VARCHAR(50),
+    description_id VARCHAR(200),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product (
+    id SERIAL,
+    unitary_price INT,
+    category_id INT NOT NULL,
+    description_product VARCHAR(200),
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE invoice_detail_list (
+    invoice_id INT NOT NULL,
+    product_id INT NOT NULL,
+    amount INT,
+    unitary_price INT,
+    product_total INT,
+    FOREIGN KEY (invoice_id) REFERENCES invoice(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
